57909b587a440e873d4d2002f1d84905f76560e5
Darren Yin <darrenyin0110@yahoo.com>
2016-02-29 17:48:52 -0500

    beacon stuff, unfinished

---
+++ AndroidAR/app/src/main/java/com/example/livelyturtle/androidar/Beacon.java
@@ -0,0 +1,95 @@
+package com.example.livelyturtle.androidar;
+
+import com.example.livelyturtle.androidar.MoverioLibraries.Moverio3D;
+import com.example.livelyturtle.androidar.MoverioLibraries.Moverio3D.*;
+import java.util.ArrayList;
+
+/**
+ * Created by Darren on 2/20/16.
+ */
+public class Beacon extends WorldObject{
+
+    static final int INDEX_COUNT = 240;
+    static final int VERTEX_COUNT = 80;
+
+    Coordinate textCoord;
+
+    //NOTE: Arraylist coordinates only holds 1 coordinate
+    public Beacon(String name, ArrayList<Coordinate> coordinates){
+        super(name, coordinates, 15.0f);
+
+        double lat = 0;
+        double lon = 0;
+        for (Coordinate coord : this.coordinates) {
+            lat += coord.latitude;
+            lon += coord.longitude;
+        }
+        lat -= this.coordinates.get(0).latitude;
+        lon -= this.coordinates.get(0).longitude;
+    }
+
+    public ArrayList<Moverio3D.Vector> vectors() {
+
+        ArrayList<Moverio3D.Vector> vectors = new ArrayList<Moverio3D.Vector>();
+
+        //Store top cap vertices (vertex 0-19)
+        for(int i = 0; i < 20; i++){
+            Moverio3D.rotateYAxis((i*18.0f), (float) Math.toRadians(deg))
+            vectors.add(Vector.of((float) (i*18.0f), 0, 1));
+        }
+
+        //Store bottom cap vertices (vertex 20-39)
+
+        //Store top cap vertices (vertex 40-59)
+
+        //store bottom cap vertices (vertex 60-79)
+
+
+        return vectors;
+    }
+
+    //order of vertices
+    public ArrayList<Short> vector_order(){
+
+        ArrayList<Short> order = new ArrayList<Short>();
+
+        //top cap indices, 0-53
+        for(int i = 0; i < 18; i++){
+            order.add((short) 0);
+            order.add((short) (i+1));
+            order.add((short) (i+2));
+        }
+
+        //bottom cap indices, 54-107
+        for(int i = 18; i < 36; i++){
+            order.add((short) 20);
+            order.add((short) (i+3));
+            order.add((short) (i+4));
+        }
+
+        //indices for barrel of cylinder
+        for(int i = 0; i < 19; i++){
+            order.add((short) (i+40));
+            order.add((short) (i+41));
+            order.add((short) (i+60));
+            order.add((short) (i+41));
+            order.add((short) (i+61));
+            order.add((short) (i+60));
+        }
+
+        //build last quad of barrel, which has looping indices
+        order.add((short)59);
+        order.add((short)40);
+        order.add((short)79);
+        order.add((short)40);
+        order.add((short)60);
+        order.add((short)79);
+
+        return order;
+    }
+
+    public Coordinate getTextCoord() {
+        return textCoord;
+    }
+
+}
